a1ccb8fde6c1ada5c44e0c219e0d9e3494512c73
jukemori <jukemori@gmail.com>
2023-09-29 18:00:48 +0900

    set types for login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,39 +1,54 @@
-import { useState } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+
+interface FormData {
+  email: string;
+  password: string;
+}
+
+interface UserData {
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at: string | null;
+  created_at: string;
+  updated_at: string;
+  // Add other properties if necessary
+}
 
 const Login = () => {
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     email: "",
     password: "",
   });
 
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
   const navigate = useNavigate();
 
-  const handleChange = (e) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleLogin = async (e) => {
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post(
-        "http://localhost:8000/api/login",
-        formData
-      );
+      const response: AxiosResponse<{
+        user: UserData; // Define the shape of your user data
+        token: string;
+      }> = await axios.post("http://localhost:8000/api/login", formData);
 
       const user = response.data.user;
-      const token = response.data.token; // Extract the token from the response
+      const token = response.data.token;
 
       localStorage.setItem("user", JSON.stringify(user));
-      localStorage.setItem("token", token); // Store the token in localStorage
+      localStorage.setItem("token", token);
       setLoggedIn(true);
 
       console.log("User logged in:", user);
-      console.log("Token:", token); // Now, the token should not be null
+      console.log("Token:", token);
       navigate("/projects");
     } catch (error) {
       console.error("Login failed:", error);
@@ -52,12 +67,14 @@ const Login = () => {
               name="email"
               placeholder="Email"
               onChange={handleChange}
+              value={formData.email}
             />
             <input
               type="password"
               name="password"
               placeholder="Password"
               onChange={handleChange}
+              value={formData.password}
             />
             <button type="submit">Login</button>
           </form>
