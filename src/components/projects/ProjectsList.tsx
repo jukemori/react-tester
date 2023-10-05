d3594bb9716572ba46b34cfc2bdeea6161ad6e7e
jukemori <jukemori@gmail.com>
2023-10-05 13:13:46 +0900

    initial commit

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -1,63 +1,42 @@
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
-  fetchProjects,
-  deleteProject,
-  updateProjectName,
-  createProject,
-} from "../../api/projectApi";
+  FETCH_PROJECTS,
+  DELETE_PROJECT,
+  UPDATE_PROJECT_NAME,
+  CREATE_PROJECT,
+  startEditingProject,
+} from "../../redux/project/projectActions"; // Adjust the import path
+
 import ProjectItem from "./ProjectItem";
+import store from "../../redux/store";
 
 function ProjectList() {
-  const [projects, setProjects] = useState<Project[]>([]);
+  const projects = useSelector((state: RootState) => state.projects.projects);
+  const dispatch = useDispatch();
+
   const [projectNames, setProjectNames] = useState<{ [key: number]: string }>(
     {}
   );
   const [newProject, setNewProject] = useState<string>("");
-  const [editMode, setEditMode] = useState<{ [key: number]: boolean }>({});
   const token: string = localStorage.getItem("token") || "";
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (!token) {
-          // Handle unauthenticated state as needed
-          return;
-        }
-
-        const response: Project[] = await fetchProjects(token);
-        setProjects(response);
-        const initialProjectNames: { [key: number]: string } = {};
-        response.forEach((project) => {
-          initialProjectNames[project.id] = project.name;
-        });
-        setProjectNames(initialProjectNames);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, [token]);
+    dispatch({ type: FETCH_PROJECTS, payload: token }); // Corrected action type
+  }, [dispatch, token]);
 
   const startEditingProject = (projectId: number) => {
-    setEditMode((prevEditMode) => ({ ...prevEditMode, [projectId]: true }));
+    // Dispatch the startEditingProject action
+    dispatch(startEditingProject(projectId));
   };
 
   const updateProject = async (projectId: number) => {
     try {
       const newName = projectNames[projectId];
-      const updatedProject = await updateProjectName(projectId, newName, token);
-      setProjects((prevProjects) =>
-        prevProjects.map((project) =>
-          project.id === projectId
-            ? { ...project, name: updatedProject.name }
-            : project
-        )
-      );
-      setEditMode((prevEditMode) => ({
-        ...prevEditMode,
-        [projectId]: false,
-      }));
+      await dispatch({
+        type: UPDATE_PROJECT_NAME,
+        payload: { projectId, newName, token },
+      }); // Corrected action type
     } catch (error) {
       console.error(error);
     }
@@ -65,10 +44,7 @@ function ProjectList() {
 
   const deleteProjectById = async (projectId: number) => {
     try {
-      await deleteProject(projectId, token);
-      setProjects((prevProjects) =>
-        prevProjects.filter((project) => project.id !== projectId)
-      );
+      await dispatch({ type: DELETE_PROJECT, payload: { projectId, token } }); // Corrected action type
     } catch (error) {
       console.error(error);
     }
@@ -83,8 +59,10 @@ function ProjectList() {
         name: newProject,
         user_id: userId,
       };
-      const createdProject = await createProject(newProjectData, token);
-      setProjects((prevProjects) => [...prevProjects, createdProject]);
+      await dispatch({
+        type: CREATE_PROJECT,
+        payload: { newProjectData, token },
+      }); // Corrected action type
       setNewProject("");
     } catch (error) {
       console.error(error);
@@ -105,9 +83,9 @@ function ProjectList() {
           <ProjectItem
             key={project.id}
             project={project}
-            isEditing={!!editMode[project.id]}
+            isEditing={!!projectNames[project.id]}
             projectNames={projectNames}
-            onEdit={startEditingProject}
+            onEdit={() => startEditingProject(project.id)}
             onUpdate={updateProject}
             onDelete={deleteProjectById}
             onNameChange={handleProjectNameChange}
