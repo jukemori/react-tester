45450da2114d01774197650825f5606729a305e8
jukemori <jukemori@gmail.com>
2023-10-06 10:18:10 +0900

    lol

--- src/redux/project/projectReducer.ts
+++ src/redux/project/projectReducer.ts
@@ -4,8 +4,11 @@ import {
   FETCH_PROJECTS_SUCCESS,
   FETCH_PROJECTS_FAILURE,
   DELETE_PROJECT_SUCCESS,
+  DELETE_PROJECT_FAILURE, // Add DELETE_PROJECT_FAILURE
   UPDATE_PROJECT_NAME_SUCCESS,
+  UPDATE_PROJECT_NAME_FAILURE, // Add UPDATE_PROJECT_NAME_FAILURE
   CREATE_PROJECT_SUCCESS,
+  CREATE_PROJECT_FAILURE, // Add CREATE_PROJECT_FAILURE
   ProjectActionTypes,
 } from "./projectActions";
 
@@ -37,6 +40,14 @@ const projectReducer = (
       return {
         ...state,
         error: null,
+      };
+    // Handle error cases
+    case DELETE_PROJECT_FAILURE:
+    case UPDATE_PROJECT_NAME_FAILURE:
+    case CREATE_PROJECT_FAILURE:
+      return {
+        ...state,
+        error: action.payload, // Set the error to the payload (error object)
       };
     default:
       return state;
